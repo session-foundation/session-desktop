dc77249943d68541c9baedd7f5330b7c1393157d
yougotwill <yougotwill94@gmail.com>
2025-02-17 11:38:05 +1100

    fix: make sure debug latest release always uses latest channel

--- ts/components/dialog/debug/components.tsx
+++ ts/components/dialog/debug/components.tsx
@@ -83,7 +83,10 @@ export const DebugActions = () => {
               return;
             }
             setLoadingLatestRelease(true);
-            const versionNumber = await getLatestReleaseFromFileServer(userEd25519SecretKey);
+            const versionNumber = await getLatestReleaseFromFileServer(
+              userEd25519SecretKey,
+              'latest'
+            );
             setLoadingLatestRelease(false);
 
             if (versionNumber) {
