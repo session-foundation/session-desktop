c817db468a9b4d12e26cec6f9eb6df6ef1ce38da
yougotwill <yougotwill94@gmail.com>
2025-03-24 15:50:00 +1100

    fix: in release arch script we trust the user argument and not host info
    so we use x64 instead of x86_64 for intel builds

--- build/setup-mac-release-arch.sh
+++ build/setup-mac-release-arch.sh
@@ -9,7 +9,7 @@ echo "Setup release metadata for $ARCH architecture"
 if [[ "$ARCH" == "arm64" ]]; then
   ls -t $RELEASE_DIR/latest-mac.yml | head -n 1 | xargs -I {} mv {} $RELEASE_DIR/latest-mac-$ARCH.yml
   echo "Created latest-mac-$ARCH.yml"
-elif [[ "$ARCH" == "x86_64" ]]; then
+elif [[ "$ARCH" == "x64" ]]; then
   ls -t $RELEASE_DIR/latest-mac.yml | head -n 1 | xargs -I {} mv {} $RELEASE_DIR/latest-mac-x64.yml
   echo "Created latest-mac-x64.yml"
 else
