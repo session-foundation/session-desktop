6e1762d260bcae4cc0efa3abfbfc807a52a09066
andyo <javabudd@gmail.com>
2025-08-09 15:00:34 -0600

    chore: add unit tests for updateSwarmNodesForPubkey, clearOutAllSnodesNotInPool, saveConversation

--- ts/test/data/data_test.ts
+++ ts/test/data/data_test.ts
@@ -7,6 +7,8 @@ import * as dataInit from '../../data/dataInit';
 import { GuardNode } from '../../data/types';
 import { Storage } from '../../util/storage';
 import * as cryptoUtils from '../../session/crypto';
+import { ConversationAttributes } from '../../models/conversationAttributes';
+import { SaveConversationReturn } from '../../types/sqlSharedTypes';
 
 describe('data', () => {
   beforeEach(() => {
@@ -18,6 +20,9 @@ describe('data', () => {
     channels.getItemById = () => {};
     channels.createOrUpdateItem = () => {};
     channels.getSwarmNodesForPubkey = () => {};
+    channels.updateSwarmNodesForPubkey = () => {};
+    channels.clearOutAllSnodesNotInPool = () => {};
+    channels.saveConversation = () => {};
   });
 
   afterEach(() => {
@@ -112,6 +117,92 @@ describe('data', () => {
       expect(getSwarmNodesForPubkeyStub.calledOnce).to.be.true;
       expect(getSwarmNodesForPubkeyStub.calledWith(expectedPubkey)).to.be.true;
       expect(expectedSwarmNodes).to.deep.equal(actualSwarmNodes);
+    });
+  });
+
+  describe('updateSwarmNodesForPubkey', () => {
+    it('updates swarm nodes for pubkey', async () => {
+      const updateSwarmNodesForPubkeyStub = Sinon.stub(channels, 'updateSwarmNodesForPubkey');
+      const expectedPubkey = 'test_pubkey_123';
+      const expectedSnodeEdKeys = ['node1', 'node2', 'node3'];
+
+      const result = await Data.updateSwarmNodesForPubkey(expectedPubkey, expectedSnodeEdKeys);
+
+      expect(updateSwarmNodesForPubkeyStub.calledOnce).to.be.true;
+      expect(updateSwarmNodesForPubkeyStub.calledWith(expectedPubkey, expectedSnodeEdKeys)).to.be
+        .true;
+      expect(result).to.be.undefined;
+    });
+  });
+
+  describe('clearOutAllSnodesNotInPool', () => {
+    it('clears out all snodes not in pool', async () => {
+      const clearOutAllSnodesNotInPoolStub = Sinon.stub(channels, 'clearOutAllSnodesNotInPool');
+      const expectedEdKeysOfSnodePool = ['snode1', 'snode2', 'snode3'];
+
+      const result = await Data.clearOutAllSnodesNotInPool(expectedEdKeysOfSnodePool);
+
+      expect(clearOutAllSnodesNotInPoolStub.calledOnce).to.be.true;
+      expect(clearOutAllSnodesNotInPoolStub.calledWith(expectedEdKeysOfSnodePool)).to.be.true;
+      expect(result).to.be.undefined;
+    });
+  });
+
+  describe('saveConversation', () => {
+    it('saves conversation with normal data', async () => {
+      const conversationData: ConversationAttributes = {
+        id: 'test_convo_123',
+        active_at: 1234567890,
+        type: 'private',
+      } as ConversationAttributes;
+
+      const expectedReturn: SaveConversationReturn = {
+        unreadCount: 0,
+        mentionedUs: false,
+        lastReadTimestampMessage: null,
+      };
+
+      const saveConversationStub = Sinon.stub(channels, 'saveConversation').resolves(
+        expectedReturn
+      );
+      const result = await Data.saveConversation(conversationData);
+
+      expect(saveConversationStub.calledOnce).to.be.true;
+      expect(saveConversationStub.calledWith(conversationData)).to.be.true;
+      expect(result).to.deep.equal(expectedReturn);
+    });
+
+    it('updates active_at when it is -Infinity', async () => {
+      const mockNow = 9876543210;
+      const dateNowStub = Sinon.stub(Date, 'now').returns(mockNow);
+
+      const conversationData: ConversationAttributes = {
+        id: 'test_convo_123',
+        active_at: -Infinity,
+        type: 'private',
+      } as ConversationAttributes;
+
+      const expectedCleanedData = {
+        id: 'test_convo_123',
+        active_at: mockNow,
+        type: 'private',
+      };
+
+      const expectedReturn: SaveConversationReturn = {
+        unreadCount: 0,
+        mentionedUs: false,
+        lastReadTimestampMessage: null,
+      };
+
+      const saveConversationStub = Sinon.stub(channels, 'saveConversation').resolves(
+        expectedReturn
+      );
+      const result = await Data.saveConversation(conversationData);
+
+      expect(saveConversationStub.calledOnce).to.be.true;
+      expect(saveConversationStub.calledWith(expectedCleanedData)).to.be.true;
+      expect(result).to.deep.equal(expectedReturn);
+      expect(dateNowStub.calledOnce).to.be.true;
     });
   });
 
