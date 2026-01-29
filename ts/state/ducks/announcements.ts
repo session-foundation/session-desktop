79a7431f999c5796ce85380d686b48c0060466f9
Audric Ackermann <audric@getsession.org>
2026-01-29 14:07:57 +1100

    fix: remove deb apt warning on start for linux users

--- ts/state/ducks/announcements.ts
+++ ts/state/ducks/announcements.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { LUCIDE_ICONS_UNICODE } from '../../components/icon/lucide';
 import type { TrArgs } from '../../localization/localeTools';
 
 const debNeedsBreakingUpdate = 'debNeedsBreakingUpdate';
@@ -26,29 +25,6 @@ export const announcementSlice = createSlice({
   name: 'announcements',
   initialState: initialAnnouncementState,
   reducers: {
-    addDebNeedsBreakingUpdate(state, action: PayloadAction<boolean>) {
-      const foundAt = state.details.findIndex(a => a.key === debNeedsBreakingUpdate);
-
-      if (!action.payload) {
-        if (foundAt !== -1) {
-          state.details.splice(foundAt, 1);
-        }
-        return;
-      }
-
-      if (foundAt === -1) {
-        state.details.push({
-          key: debNeedsBreakingUpdate,
-          timestamp: Date.now(),
-          titleArgs: { token: 'announcementDebBreakingUpdateTitle' as const },
-          descriptionArgs: {
-            token: 'announcementDebBreakingUpdateDescription' as const,
-            icon: LUCIDE_ICONS_UNICODE.EXTERNAL_LINK_ICON,
-          },
-          link: 'https://getsession.org/rotating-keys-for-session-repos',
-        });
-      }
-    },
     closeByKey(state, action: PayloadAction<AnnouncementKeys>) {
       const foundAt = state.details.findIndex(a => a.key === action.payload);
 
